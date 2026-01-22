fa99160939654507a3b9fa79d0427e776a389588
M HUZAIFA ASLAM <aeaiauprps@gmail.com>
2026-01-22 15:49:52 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,17 +3,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 import { getFirestore, doc, setDoc, getDoc, collection, addDoc, getDocs, query, orderBy, updateDoc, deleteDoc, where } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_j7TFzGpeYbZk_YOrrbD8XtLL1cwAH18",
-    authDomain: "e-commerce-6ba77.firebaseapp.com",
-    projectId: "e-commerce-6ba77",
-    storageBucket: "e-commerce-6ba77.firebasestorage.app",
-    messagingSenderId: "130064601408",
-    appId: "1:130064601408:web:eedabaea945162fde8aaa5",
-    measurementId: "G-8CFSVVQP0P"
+    //Your API
   };
   
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
 
 export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, db, doc, setDoc, getDoc, collection, addDoc, getDocs, query, orderBy, updateDoc, deleteDoc, where };
